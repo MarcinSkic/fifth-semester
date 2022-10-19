b2017c310218c360bdf9bfdd9c6319d7439d5aff
MarcinSkic <marcinskicoficjal@gmail.com>
2022-10-19 13:09:09 +0200

    [adv-oop-in-cpp] Start Lab2

---
+++ Lab1/Ex1Task2/Animal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marcinskic on 10/18/22.
+//
+
+#include "Animal.h"
---
+++ Lab1/Ex1Task2/Animal.h
@@ -0,0 +1,14 @@
+//
+// Created by marcinskic on 10/18/22.
+//
+
+#ifndef EX1TASK2_ANIMAL_H
+#define EX1TASK2_ANIMAL_H
+
+
+class Animal {
+
+};
+
+
+#endif //EX1TASK2_ANIMAL_H
