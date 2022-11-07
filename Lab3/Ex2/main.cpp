4608e4021d733f2b6a01ef0538316b200e86a355
Marcin Skic <marcinskicoficjal@gmail.com>
2022-11-07 19:02:07 +0100

    [adv-oop-in-cpp] Add comments for report

--- Lab3/Ex2/main.cpp
+++ Lab3/Ex2/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-template <typename T>
+template <typename T>   //Funkcja szablonowa, szukająca elementu maksymalnego w tablicy
 void findMin(T* arr, int size){
     T min = arr[0];
     for(auto i = 1; i < size; i++){
@@ -14,10 +14,10 @@ void findMin(T* arr, int size){
 }
 
 int main() {
-    int arrInt[5] = {1,2,351,2,5};
+    int arrInt[5] = {54,2,351,1,5};
     float arrFloat[4] = {0.2,0.5,512,12};
 
-    findMin(arrInt,5);
-    findMin(arrFloat,4);
+    findMin(arrInt,5);  //Działa zarówno na int
+    findMin(arrFloat,4);    //Jak i na float
     return 0;
 }
--- Lab3/Ex3/Array.h
+++ Lab3/Ex3/Array.h
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-template <typename T>
+template <typename T>   //Szablon klasy przechowującej tablicę elementów i wykonującej operacje na niej
 class Array {
 private:
     T* array;
@@ -26,9 +26,9 @@ public:
         array = new T[10];
     }
     virtual ~Array() {
-        delete array;
+        delete array;   //Usunięcie tablicy prostych elementów (int, float, etc.)
     }
-    void sort() {
+    void sort() {   //Sortowanie bąbelkowe tablicy
         for(int i = 0; i < index-1; i++){
             for(int x = 0; x < index-i-1; x++){
                 if(array[x] > array[x+1]){
@@ -66,7 +66,36 @@ public:
     }
 };
 
+//Specjalizacja wybranych metod dla typu string
 template<>
+void Array<string>::sort() {
+    for(int i = 0; i < index-1; i++){
+        for(int x = 0; x < index-i-1; x++){
+            if(array[x].length() > array[x+1].length()){
+                array[x].swap(array[x+1]);
+            }
+        }
+    }
+}
+
+template<>
+string Array<string>::getMax() {
+    string max = array[0];
+    for(int i = 1; i < index; i++){
+        if(max.length() < array[i].length()){
+
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+template<>
+Array<string>::~Array() {
+    delete []array; //Usunięcie tablicy obiektów
+}
+
+/*template<>
 class Array<string>{
 private:
     string* array;
@@ -84,7 +113,7 @@ public:
         array = new string [10];
     }
     virtual ~Array() {
-        delete []array; //THISS!ERWwHETUHIOSJFH!@!>@?!
+        delete []array; //Usunięcie tablicy obiektów
     }
 
     void sort() {
@@ -106,6 +135,7 @@ public:
         }
         return max;
     }
+
     void show() {
         cout<<"Array: ";
         for(int i = 0; i < index; i++){
@@ -123,5 +153,5 @@ public:
         return array[index];
     }
 };
-
+*/
 #endif //EX3_ARRAY_H
