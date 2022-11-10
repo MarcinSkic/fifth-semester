d0b08287927fd78b5284bc6e7e3b105e5f0fec09
MarcinSkic <marcinskicoficjal@gmail.com>
2022-11-10 15:40:19 +0100

    [adv-oop-in-cpp] Finish ex2 lab4

---
+++ Lab4/Ex2/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+void showElement(T &elem){
+    cout<<elem<<" ";
+}
+
+int main() {
+    srand(time(nullptr));
+
+    int listSize = (rand() % 500 + 1);
+    list<int> list;
+
+    for(auto i = 0; i < listSize; i++) {
+        int elem = (rand() % 201 - 100);
+        if(elem < 0){
+            list.push_back(elem);
+        } else {
+            list.push_front(elem);
+        }
+    }
+
+    for_each(list.begin(),list.end(), showElement<int>);
+    return 0;
+}
