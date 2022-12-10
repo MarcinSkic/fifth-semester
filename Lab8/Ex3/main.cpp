ad707231fc59bf777d872a84fadd52b5a53767a7
MarcinSkic <marcinskicoficjal@gmail.com>
2022-12-10 15:00:20 +0100

    [adv-oop-in-cpp] Begin Lab10 on uni

--- Lab8/Ex3/main.cpp
+++ Lab8/Ex3/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //students.sort (by: { $0.average < $1.average})
+
+    std::vector<int> inty = {5,34,123,65,3,12,-124};
+    int granica = 5;
+
+    std::cout<<*std::find_if(inty.begin(),inty.end(),[&](int a1){
+        return a1 = granica;
+    });
+
+    std::for_each(inty.begin(),inty.end(),[](int el){
+        std::cout<<el<<" ";
+    });
     return 0;
 }
